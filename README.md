330af2a869fbee1b686e9d91039e120a0e6632c2
Jeremy Helms <digitaljhelms@gmail.com>
2022-11-15 22:25:56 -0600

    Enhancements & maintenance
    * Adds CLI options to support multiple sitemap schemas
    * Adds ascending & descending sorting option
    * Adds ignore list support to exclude links found in sitemap.xml
    * Adds support for accessing sitemap.xml within a basic auth realm
    * Adds option to specify timestamp node (if exists)
    * Updates node-json2html package to latest version

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+## Usage
+
 `npm run start -- --url=http://www.example.com/sitemap.xml` would fetch `sitemap.xml`, transform the XML into a simple HTML page of links, and serve the output over HTTP port 3000.
 
-```XML
+```xml
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
   <url>
@@ -10,34 +12,65 @@
 </urlset>
 ```
 
-_Note: The `lastmod` node is ignored._
-
 The rendered HTML output:
 
-```HTML
-<html>
+```html
+<html lang="en">
+  <head>
+    <title>HTMLMAP</title>
+    <style>body { font-family: sans-serif; }</style>
+  </head>
   <body>
-    <ol>
-      <li><a href="http://www.example.com/foo.html">http://www.example.com/foo.html</a></li>
-    </ol>
+    <main>
+      <ol>
+        <li><a href="http://www.example.com/foo.html">http://www.example.com/foo.html</a></li>
+      </ol>
+    </main>
   </body>
 </html>
 ```
 
-`urlset` and `url` are the default nodes searched for in the XML structure, however, additional CLI arguments can be specified:
+## Custom XML Structure
 
-* `root`: The root/top-level document node
-* `entry`: The parent node for each URL
+`urlset` and `url` are the default nodes searched for in the XML structure, however, additional CLI arguments can be specified.
 
 These arguments are useful for addressing differences in XML structure; for example:
 
-```XML
-<sitemapindex xmlns="http://www.google.com/schemas/sitemap/0.84">
+```xml
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
-    <loc>https://www.google.com/gmail/sitemap.xml</loc>
+    <loc>https://www.example.com/foo.html</loc>
+    <pubDate>2022-01-01T01:00:00+00:00</pubDate>
+    <changefreq>daily</changefreq>
+    <priority>1.00</priority>
   </sitemap>
 </sitemapindex>
 ```
 
 For the above structure, the following command arguments would be necessary:
-`npm run start -- --url=https://www.google.com/sitemap.xml --root=sitemapindex --entry=sitemap`
+`npm run start -- --url=https://www.example.com/sitemap.xml --root=sitemapindex --entry=sitemap`
+
+Additionally, `--sortasc` or `--sortdesc` could be used to sort by `pubDate`, `changefreq`, or `priority`.
+
+---
+
+Note: To prevent a URL found in the fetched `sitemap.xml` from being rendered in the HTML output, use the `ignore-list.txt` file – a single, fully-qualified URL, per-line, with no EOF carriage return – and pass the `--ignore` option.
+
+---
+
+## CLI Options
+
+Options must be passed after `npm run start -- `, prefixed with `--`, and a value preceeding an equal (`=`) symbol.
+
+|| Option || Default || Description ||
+| `root` | `urlset` | The root/top-level document node |
+| `entry` | `url` | The parent node for each URL |
+| `loc` | `loc` | The primary node containing each URL |
+| `sortasc` | `loc` | What XML node to use for sorting (ascending) |
+| `sortdesc` | none | What XML node to use for sorting (descending) |
+| `timestamp` | none | To display the date/time stamp of last modified or published (if it exists in the XML) pass the value of the XML node itself (ex: `--dtstamp=lastmod`) |
+| `user` | none | Basic authentication username |
+| `pass` | none | Basic authentication password |
+| `ignore` | none | Remove URLs found in `ignore-list.txt` file |
+
+Note: When using `sortasc` or `sortdesc` only one option can be passed; if both are passed, `sortasc` will take precedence and `sortdesc` will be ignored.
--- index.js
+++ index.js
@@ -1,47 +1,119 @@
 const argv = require('yargs').argv
+const fs = require('fs')
 const http = require('http')
-const request = require('request')
-const xml2js = require('xml2js').Parser()
 const json2html = require('node-json2html')
+const request = require('request')
+const xml2js = require('xml2js').parseString
+
+if (!argv.url) {
+  console.log('htmlmap: [ERR] No sitemap.xml URL supplied, please see README')
+  process.exit(1)
+}
 
 const options = {
   url: argv.url,
   root: (argv.root || 'urlset'),
-  entry: (argv.entry || 'url')
+  entry: (argv.entry || 'url'),
+  loc: (argv.loc || 'loc'),
+  timestamp: argv.timestamp,
+  sort: (argv.sortasc || argv.sortdesc || 'loc'),
+  reverse: (argv.sortdesc ? true : false),
+  ignore: (argv.ignore || false)
 }
 
-module.export = request(options.url, (err, res, body) => {
+if (argv.user && argv.pass) {
+  options.headers = {
+    'Authorization': 'Basic ' + Buffer.from(argv.user + ':' + argv.pass).toString('base64'),
+  }
+}
+console.log(options)
+
+Array.prototype.remove = function (key, value) {
+  const index = this.findIndex(obj => obj[key] === value);
+  return index >= 0 ? [
+    ...this.slice(0, index),
+    ...this.slice(index + 1)
+  ] : this;
+}
+
+const sort_by = (field, reverse, primer) => {
+  const key = primer ? x => primer(x[field]) : x => x[field]
+  reverse = !reverse ? 1 : -1
+
+  return (a, b) => (a = key(a), b = key(b), reverse * ((a > b) - (b > a)))
+}
+
+module.export = request.get(options, (err, res, body) => {
   if (!err && res.statusCode == 200) {
-    xml2js.parseString(body, function (err, result) {
+    xml2js(body, function (err, result) {
       let json = JSON.parse(JSON.stringify(result))[options.root][options.entry]
       // console.log(json)
+
+      json.sort(sort_by(options.sort, options.reverse, function (a) {
+        return (a ? a.toString().toUpperCase() : null )
+      }))
+      
+      // 1 FQ URL per-line, no EOF carriage return
+      if (options.ignore) {
+        try {
+          const ignoreList = fs.readFileSync('ignore-list.txt').toString().split('\n')
+
+          ignoreList.forEach(function (val) {
+            json = json.remove(options.loc, val)
+          })
+          console.log('httpmap: [JSON] Removing URLs found in ignore-list.txt file.')
+        } catch (err) {
+          console.log('httpmap: [ERR] No ignore-list.txt file found.')
+        }
+      }
 
       const transform = {
         'item': {
           '<>': 'li',
+          'text': function(obj) {
+            if (options.timestamp && obj[options.timestamp]) {
+              // console.log(obj[options.timestamp].toString())
+              return '(' + obj[options.timestamp].toString() + ') '
+            }
+          },
+          // 'text': '(${' + options.timestamp + '} )',
           'html': [{
             '<>': 'a',
-            'href': '${loc}',
-            'html': '${loc}'
+            'href': '${' + options.loc + '}',
+            'html': '${' + options.loc + '}'
           }]
         },
         'template': {
           '<>': 'html',
+          'lang': 'en',
           'html': [{
+            '<>': 'head',
+            'html': [{
+              '<>': 'title',
+              'text': 'HTMLMAP',
+            },{
+              '<>': 'style',
+              'text': 'body { font-family: sans-serif; }'
+            }]
+          },
+          {
             '<>': 'body',
             'html': [{
-              '<>': 'ol',
-              'html': function () {
-                return (json2html.transform(json, transform.item));
-              }
+              '<>': 'main',
+              'html': [{
+                '<>': 'ol',
+                'html': function () {
+                  return (json2html.render(json, transform.item))
+                }
+              }]
             }]
           }]
         }
       }
 
-      const html = json2html.transform({}, transform.template)
-
+      const html = json2html.render({}, transform.template)
       // console.log(html)
+
       http.createServer(function (req, res) {
         res.writeHead(200, {
           'Content-Type': 'text/html'
