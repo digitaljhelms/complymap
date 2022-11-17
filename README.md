ff6d28e832762841ca93a74373481e42b97f0ce3
Jeremy Helms <digitaljhelms@gmail.com>
2022-11-16 23:53:11 -0600

    Multiple enhancements
    * Adds ability to change localhost HTTP port
    * Improves ignore list support to allow for remotely hosted files
    * Improves ignore list support to specify local file override
    * Corrects HTTP content type when serving plain text output

--- README.md
+++ README.md
@@ -83,22 +83,17 @@ These arguments are useful for addressing differences in XML structure; for exam
 For the above structure, the following command arguments would be necessary:
 
 ```
-npm run start -- --url=https://www.example.com/sitemap.xml --root=sitemapindex --entry=sitemap
+npm run html -- --url=https://www.example.com/sitemap.xml --root=sitemapindex --entry=sitemap
 ```
 
 Additionally, `--sortasc` or `--sortdesc` could be used to sort by `pubDate`, `changefreq`, or `priority`.
 
----
-
-Note: To omit URLs found in the fetched `sitemap.xml` from being rendered in the output, create an `ignore-list.txt` file that includes a single fully-qualified URL, per-line, with **no EOF carriage return** at the end of the file and pass the `--ignore` option in at runtime.
-
----
-
 ## CLI Options
 
-Options must be passed after `npm run {function} -- `, prefixed with `--`, and a value preceeding an equal (`=`) symbol.
+* Options can be passed after `npm run {function} -- `
+* Each option must be prefixed with `--` and the corresponding value (if required) must be preceeded by an equal (`=`) symbol
 
-| Option | Default | Description |
+| Option | Default Value | Description |
 | ------ | ------- | ----------- |
 | `root` | `urlset` | The root/top-level document node |
 | `entry` | `url` | The parent node for each URL |
@@ -108,7 +103,21 @@ Options must be passed after `npm run {function} -- `, prefixed with `--`, and a
 | `timestamp` | none | To display the date/time stamp of last modified or published (if it exists in the XML) pass the value of the XML node itself (ex: `--timestamp=lastmod`) |
 | `user` | none | Basic authentication username |
 | `pass` | none | Basic authentication password |
-| `ignore` | none | Omit URLs found in `ignore-list.txt` file |
-| `save` | none | Saves the output to `sitemap.html` or `sitemap.txt` based on the function being executed |
+| `ignore` | none | Omit URLs found in an ignore list file (see [Ignore Lists](#ignore-lists) below) |
+| `save` | none | Saves the output to `sitemap.html` or `sitemap.txt` based on the function being executed (does not launch a local HTTP server for viewing output) |
+| `port` | `3000` | HTTP port the Node server will use for localhost (ignored if the `--save` option is used) |
 
-Note: When using `sortasc` or `sortdesc` only one option can be passed; if both are passed, `sortasc` will take precedence and `sortdesc` will be ignored.
+Note: When using `--sortasc` or `--sortdesc` only one option can be passed; if both are passed, `--sortasc` will take precedence and `--sortdesc` will be ignored.
+
+## Ignore Lists
+
+To omit URLs found in the fetched `sitemap.xml` from being rendered in the output simply create an `ignore-list.txt` file that contains a single fully-qualified URL, per-line, with **no EOF carriage return** at the end of the file, and use the `--ignore` option at runtime.
+
+Additionally, if the `--ignore` option is given a value it can be used to supply a path to either a local file named something different or even a URL to remotely hosted file that is reachable by the script at runtime.
+
+```
+npm run html -- --url=https://www.example.com/sitemap.xml --ignore=/foo/bar/baz.list
+npm run html -- --url=https://www.example.com/sitemap.xml --ignore=https://www.foo.bar/baz.list
+```
+
+Note: If a URL is provided, it must use an [RFC 3886](https://www.rfc-editor.org/rfc/rfc3986) valid URL scheme.
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const json2html = require('node-json2html')
 const pretty = require('pretty')
 const request = require('request')
 const xml2js = require('xml2js').parseString
+const noop = () => {}
 
 if (!argv.url) {
   console.log('htmlmap: [ERR] No sitemap.xml URL supplied, please see README')
@@ -24,9 +25,11 @@ const options = {
   timestamp: (argv.timestamp || null),
   sort: (argv.sortasc || argv.sortdesc || 'loc'),
   reverse: (argv.sortdesc ? true : false),
-  ignore: (argv.ignore || false),
+  ignore: (argv.ignore ? true : false),
+  ignorelist: (argv.ignore ? ((typeof argv.ignore === 'string') ? argv.ignore : 'ignore-list.txt') : null),
   save: (argv.save || false),
-  outputfile: (argv.save ? (argv.text ? 'sitemap.txt' : 'sitemap.html') : null)
+  outputfile: (argv.save ? (argv.text ? 'sitemap.txt' : 'sitemap.html') : null),
+  port: (argv.port || 3000)
 }
 
 if (argv.user && argv.pass) {
@@ -38,11 +41,11 @@ if (argv.user && argv.pass) {
 console.log('htmlmap: [ARGS] Configuration being used\n', options, '\n')
 
 Array.prototype.remove = function (key, value) {
-  const index = this.findIndex(obj => obj[key] === value);
-  return index >= 0 ? [
+  const index = this.findIndex(obj => obj[key] === value)
+  return (index >= 0 ? [
     ...this.slice(0, index),
     ...this.slice(index + 1)
-  ] : this;
+  ] : this)
 }
 
 const sort_by = (field, reverse, primer) => {
@@ -52,28 +55,55 @@ const sort_by = (field, reverse, primer) => {
   return (a, b) => (a = key(a), b = key(b), reverse * ((a > b) - (b > a)))
 }
 
+const validURL = (val) => {
+  let url
+  try {
+    url = new URL(val)
+  } catch (_) {
+    return false
+  }
+  return url.protocol === "http:" || url.protocol === "https:"
+}
+
+// 1 FQ URL per-line, no EOF carriage return
+let ignoreList = ''
+if (options.ignore) {
+  if (validURL(options.ignorelist)) {
+    request.get(options.ignorelist, (err, res, body) => {
+      if (!err && res.statusCode == 200) {
+        ignoreList = body.toString().split('\n')
+      } else {
+        console.log('httpmap: [WARN] Remote ignore list could not be retrieved, skipping\n')
+      }
+    })
+  } else {
+    try {
+      ignoreList = fs.readFileSync(options.ignorelist).toString().split('\n')
+    } catch (err) {
+      console.log('httpmap: [WARN] Ignore list not found or not accessible, skipping\n')
+    }
+  }
+}
+
 console.log('htmlmap: [HTTP] Retrieving sitemap.xml\n')
 
-module.export = request.get(options, (err, res, body) => {
+module.exports.htmlmap = request.get(options, (err, res, body) => {
   if (!err && res.statusCode == 200) {
-    xml2js(body, function (err, result) {
+    xml2js(body, { explicitArray: false }, function (err, result) {
       let json = JSON.parse(JSON.stringify(result))[options.root][options.entry]
 
       json.sort(sort_by(options.sort, options.reverse, function (a) {
         return (a ? a.toString().toUpperCase() : null )
       }))
 
-      // 1 FQ URL per-line, no EOF carriage return
       if (options.ignore) {
         try {
-          const ignoreList = fs.readFileSync('ignore-list.txt').toString().split('\n')
-
-          ignoreList.forEach(function(val) {
+          ignoreList.forEach(function (val) {
             json = json.remove(options.location, val)
           })
-          console.log('httpmap: [JSON] Removing URLs found in ignore-list.txt file.')
+          console.log(`httpmap: [JSON] Removed URLs found in ${options.ignorelist}\n`)
         } catch (err) {
-          console.log('httpmap: [ERR] No ignore-list.txt file found.')
+          console.log('httpmap: [WARN] Ignore list could not be properly parsed, skipping\n')
         }
       }
 
@@ -83,7 +113,7 @@ module.export = request.get(options, (err, res, body) => {
         const transform = {
           'item': {
             '<>': 'li',
-            'text': function(obj) {
+            'text': function (obj) {
               if (options.timestamp && obj[options.timestamp]) {
                 return '(' + obj[options.timestamp].toString() + ') '
               }
@@ -124,23 +154,24 @@ module.export = request.get(options, (err, res, body) => {
 
         output = pretty(json2html.render({}, transform.template))
       } else {
-        json.forEach(function(val) {
+        json.forEach(function (val) {
           output += val[options.location].toString() + '\n'
         })
       }
 
       if (options.save) {
         fs.writeFile(options.outputfile, output.trim(), (err) => {
-          if (err) throw err;
+          if (err) throw err
           console.log(`htmlmap: [SAVE] Output saved to ${options.outputfile}`)
         })
       } else {
-        http.createServer(function(req, res) {
+        const contentType = (options.format === 'html' ? 'text/html' : 'text/plain')
+        http.createServer(function (req, res) {
           res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': contentType
           });
           res.end(output)
-        }).listen(3000)
+        }).listen(options.port)
 
         console.log('htmlmap: [SERV] Listening on http://localhost:3000')
       }
